7fa2c60ef00ed60dd654761016f29210f0728010
xuecheng-han <hanxuecheng@wealthengine.cn>
2023-06-25 17:03:23 +0800

    init code

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# wealthengine
+# Sealoder
+
+Sealoader 是一个在 seafile 中下载文件的程序
+## 环境准备
+```bash
+pip install seafile-api
+```
+
+## 使用示例
+```python
+from sealoder import sealoder
+
+sealoder.download_folder(folder="case_name", to_path="./existing_folder")
+```
+
+Sealoader is a program for downloading files in seafile
+## Environmental preparation
+```bash
+pip install seafile-api
+```
+
+## Use example
+```python
+from sealoder import sealoder
+
+sealoder.download_folder(folder="case_name", to_path="./existing_folder")
+```
+
+
+
+
---
+++ sealoder.py
@@ -0,0 +1,46 @@
+import os
+import seafileapi
+from seafileapi.exceptions import DoesNotExist
+
+
+class DownloadSeaFile:
+
+    def __init__(self, **kwargs):
+        server_link = kwargs.get("server_link", "https://oss.wealthengine.cn")
+        username = kwargs.get("username", "username@wealthengine.cn")
+        password = kwargs.get("password", "xxxxxxxx")
+
+        self.filename = kwargs.get("filename", None)
+        self.client = seafileapi.connect(server_link, username, password)
+
+    def _write_file(self, to_path, file_name, file_obj):
+        with open(f'{to_path}/{file_name}', 'wb') as files:
+            files.write(file_obj.get_content())
+
+    def download_folder(self, folder, to_path, single_file=None):
+        is_exists = os.path.exists(to_path)
+        if not is_exists:
+            raise Exception(f"{to_path} not exits")
+
+        repo_list = self.client.repos.list_repos()
+        repo_name_dict = {repo.name: repo.id for repo in repo_list}
+        if folder not in repo_name_dict.keys() or folder is None:
+            raise Exception(f"{folder} not exits in seafile or you do not have access to this directory")
+        repo = self.client.repos.get_repo(repo_name_dict[folder])
+        try:
+            dirs = repo.get_dir("/")
+            if single_file is not None:
+                file_obj = repo.get_file(f'/{single_file}')
+                file_name = file_obj.path
+                self._write_file(to_path, file_name, file_obj)
+                return True
+            for entries in dirs.entries:
+                file_name = entries.path
+                file_obj = repo.get_file(file_name)
+                self._write_file(to_path, file_name, file_obj)
+            return True
+        except DoesNotExist:
+            raise Exception(f"dir not exits in seafile or you do not have access to this directory")
+
+
+sealoder = DownloadSeaFile()
